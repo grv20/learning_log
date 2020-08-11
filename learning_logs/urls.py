305b5ecf171fdc59a850a1c3daecd817915889ff
grv20 <windroid20@gmail.com>
2020-08-11 08:48:02 +0530

    added entry function

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
 path('', views.index, name='index'),
 path('topics/', views.topics, name='topics'),
 path('topics/(?P<topic_id>\d+)/', views.topic, name='topic'),
+# Page for adding a new topic
+path('new_topic/', views.new_topic, name='new_topic'),
+# Page for adding a new entry
+path('new_entry/(?P<topic_id>\d+)/', views.new_entry, name='new_entry'),
 ]
